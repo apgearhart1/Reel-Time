1cf3bfb1e65b2bf0240dcd6ca5ff89694c8bdacb
apgearhart1 <apgearhart1@gmail.com>
2019-07-31 12:47:30 -0500

    Added get-location button

--- lib/MapTab.dart
+++ lib/MapTab.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'dart:async';
 
 class MapTab extends StatelessWidget {
   GoogleMapController mapController;
-
-
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  Completer<GoogleMapController> _controller = Completer();
 
   var location = new Location();
-
 
   final LatLng _center = const LatLng(46, 32);
 
@@ -21,7 +16,9 @@ class MapTab extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
@@ -29,7 +26,33 @@ class MapTab extends StatelessWidget {
           zoomGesturesEnabled: true,
           myLocationEnabled: true,
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _currentLocation,
+          label: Text('button'),
+          icon: Icon(Icons.location_on),
+        ),
       ),
     );
+  }
+
+
+
+  void _currentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    LocationData currentLocation;
+    var location = new Location();
+    try {
+      currentLocation = await location.getLocation();
+    } on Exception {
+      currentLocation = null;
+    }
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 17.0,
+      ),
+    ));
   }
 }
